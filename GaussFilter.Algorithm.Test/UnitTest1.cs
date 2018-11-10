065eaee57393195b614447a143a6a834779b0a20
piotpac845 <piotpac845@student.polsl.pl>
2018-11-10 19:40:56 +0100

    Added gauss blur algorithm

---
+++ GaussFilter.Algorithm.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using Moq;
+
+namespace GaussFilter.Algorithm.Test
+{
+    [TestFixture]
+    public class GaussFilterTest
+    {
+       
+        [Test]
+        public void ShouldCreateProperMask()
+        {
+            // given
+            var imageMock = new Mock<Bitmap>();
+            imageMock.Setup(image => image.Height).Returns(10);
+            imageMock.Setup(image => image.Width).Returns(10);
+            int maskSize = 3;
+            double radius = 1;
+
+            // when
+            GaussFilter filter = new GaussFilter(maskSize, radius, imageMock.Object);
+
+            // then
+
+        }
+    }
+}
--- GaussFilter.Algorithm/GaussFilter.cs
+++ GaussFilter.Algorithm/GaussFilter.cs
@@ -1,29 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GaussFilter.Algorithm
 {
-    class GaussFilter
+    public class GaussFilter
     {
-        public GaussFilter(int maskSize, double gaussRadius, string imagePath)
+        private const double PI = 3.1415;
+        private const double e = 2.7182;
+        private double maskValuesSum;
+
+        public GaussFilter(int maskSize, double gaussRadius, Bitmap image)
         {
             MaskSize = maskSize;
             GaussRadius = gaussRadius;
-            ImagePath = imagePath;
+            Image = image;
+            FilteredImage = CreateNewBitmap();
+            CreateMask();
+            maskValuesSum = Mask.Sum(m => m);
         }
 
         public int MaskSize { get; }
         public double GaussRadius { get; }
-        public string ImagePath { get; }
+        public IList<double> Mask { get; private set; }
+        public Bitmap Image { get; private set; }
+        public Bitmap FilteredImage { get; private set; }
 
-        public  void Apply()
+    
+        private Bitmap CreateNewBitmap()
         {
-            Bitmap image = new Bitmap(ImagePath, true);
-            
+            var newWidth = Image.Width - (MaskSize - 1);
+            var newHeigth = Image.Height - (MaskSize - 1);
+            return new Bitmap(newWidth, newHeigth);
         }
+
+        private void CreateMask()
+        {
+            int positionDiff = ((MaskSize - 1) / 2);
+            Mask = new List<double>();
+            for (int i = 0; i < MaskSize; i++)
+            {
+                for (int j = 0; j < MaskSize; j++)
+                {
+                    Mask.Add(CalculateWeight(j - positionDiff, i - positionDiff));
+                }
+            }
+        }
+        
+        private double CalculateWeight(int x, int y)
+        {
+            double power = -(x*x+y*y) / (2* Math.Pow(GaussRadius,2));
+            double weight = 1 / (2 * PI * Math.Pow(GaussRadius, 2)) * Math.Pow(e, power);
+            return weight;
+        }
+   
+        public void Apply(string path)
+        {
+
+            int positionDiff = ((MaskSize - 1) / 2);
+
+            for (int i = 0; i < FilteredImage.Height; i++)
+            {
+                for (int j = 0; j < FilteredImage.Width; j++)
+                {
+                    int oldImageX = j + positionDiff;
+                    int oldImageY = i + positionDiff;
+                    Color color = CalculateNewPixel(oldImageX, oldImageY);
+                    FilteredImage.SetPixel(j, i, color);
+                }
+                
+            }
+            FilteredImage.Save(path);
+
+        }
+
+        private Color CalculateNewPixel(int x, int y)
+        {
+            int positionDiff = ((MaskSize - 1) / 2);
+
+            int startingX = x - positionDiff;
+            int startingY = y - positionDiff;
+
+            double R = 0d;
+            double G = 0d;
+            double B = 0d;
+            double A = 0d;
+
+            int maskCounter = 0;
+            for (int i = 0; i < MaskSize; i++)
+            {
+                for (int j = 0; j < MaskSize; j++)
+                {
+                    var oldPixel = Image.GetPixel(startingX + j, startingY + i);
+                    var weight = Mask [maskCounter];
+                    R += oldPixel.R * weight;
+                    G += oldPixel.G * weight;
+                    B += oldPixel.B * weight;
+                    A += oldPixel.A * weight;
+                    maskCounter++;
+                }
+            }
+            R /= maskValuesSum;
+            G /= maskValuesSum;
+            B /= maskValuesSum;
+            A /= maskValuesSum;
+
+            return Color.FromArgb((int)A,(int)R,(int)G,(int)B);
+        }
+
+
+
+
+
+
+
+
+     
     }
 }
